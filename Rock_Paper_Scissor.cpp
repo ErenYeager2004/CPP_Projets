d0102c3407f48c8dca82402e1527a4355de81be4
Debjit Saha <debjitsaha606@gmail.com>
2025-04-25 10:48:08 +0530

    stone paper scissor game added but not completed

---
+++ Rock_Paper_Scissor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class game{
+    private:
+        string choice[3] = {"stone","paper","scissor"};
+        string user;
+    public:
+        game(string user){
+            this->user = user;
+        }
+};
+
+int main(){
+    string user,user;
+   
+    game *obj = new game(user);
+    
+}
